5e1d1fd5342c727ca98a60915741cc09e678239f
ShashiPrakashTiwari <146014792+ShashiPrakashTiwari@users.noreply.github.com>
2024-07-24 11:58:54 +0530

    practicejavaarray

---
+++ JavaPro/src/practicejavaarray.java
@@ -0,0 +1,9 @@
+public class practicejavaarray {
+    public static void main(String[] args) {
+        String[] studentname=new String[2];
+        studentname=new String[]{"ravi","rahul"};
+    for(int i=0;i<=studentname.length;i++){
+        System.out.println(studentname[i]);
+    }
+    }
+}
