f47231954baa2efb2dc50a7b05f4f2e90e028c6b
ShashiPrakashTiwari <146014792+ShashiPrakashTiwari@users.noreply.github.com>
2024-07-24 23:04:12 +0530

    evenodd

---
+++ conditionalpro/src/evenodd.java
@@ -0,0 +1,12 @@
+public class evenodd {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            if(i%2==0){
+                System.out.println("even number"+i);
+            }
+            else{
+                System.out.println("odd number"+i);
+            }
+        }
+    }
+}
